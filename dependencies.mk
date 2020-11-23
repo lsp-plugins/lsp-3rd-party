14e88d501814252e28245681d95dfae04a0264e8
sadko4u <sadko4u@gmail.com>
2020-11-23 23:47:44 +0300

    Initial commit

---
+++ dependencies.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+#           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+#
+# This file is part of lsp-dsp-lib
+#
+# lsp-dsp-lib is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# lsp-dsp-lib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lsp-dsp-lib.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+# Variables that describe dependencies
+LSP_COMMON_LIB_VERSION     := 1.0.11
+LSP_COMMON_LIB_NAME        := lsp-common-lib
+LSP_COMMON_LIB_TYPE        := src
+LSP_COMMON_LIB_URL         := https://github.com/sadko4u/$(LSP_COMMON_LIB_NAME).git
+
+LSP_TEST_FW_VERSION        := 1.0.6
+LSP_TEST_FW_NAME           := lsp-test-fw
+LSP_TEST_FW_TYPE           := src
+LSP_TEST_FW_URL            := https://github.com/sadko4u/$(LSP_TEST_FW_NAME).git
+
+ifeq ($(PLATFORM),Windows)
+  STDLIB_VERSION             := system
+  STDLIB_TYPE                := opt
+  STDLIB_LDFLAGS             := -lpthread
+else
+  STDLIB_VERSION             := system
+  STDLIB_TYPE                := opt
+  STDLIB_LDFLAGS             := -lpthread -ldl
+endif
+
---
+++ src/test/utest/headers.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-3rd-party
+ * Created on: 23 нояб. 2020 г.
+ *
+ * lsp-3rd-party is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-3rd-party is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-3rd-party. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <3rdparty/ardour/inline-display.h>
+#include <3rdparty/ladspa/ladspa.h>
+#include <3rdparty/steinberg/vst2.h>
+
+#include <lsp-plug.in/test-fw/utest.h>
+
+UTEST_BEGIN("3rdparty", headers)
+    UTEST_MAIN
+    {
+        printf("3rd party headers OK\n");
+    }
+UTEST_END
+
+
