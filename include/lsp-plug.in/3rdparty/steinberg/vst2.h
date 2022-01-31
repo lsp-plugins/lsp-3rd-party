b515a6327efb8cf1c11c32e23f77d6386e35afdb
sadko4u <sadko4u@gmail.com>
2022-01-31 23:47:08 +0300

    Updated export definitions

--- include/lsp-plug.in/3rdparty/steinberg/vst2.h
+++ include/lsp-plug.in/3rdparty/steinberg/vst2.h
@@ -112,6 +112,18 @@
     #define VSTCALLBACK
 #endif
 
+#ifdef __cplusplus
+    #define VST_SYMBOL_EXTERN extern "C"
+#else
+    #define VST_SYMBOL_EXTERN
+#endif
+
+#ifdef _WIN32
+    #define VST_SYMBOL_EXPORT VST_SYMBOL_EXTERN __declspec(dllexport)
+#else
+    #define VST_SYMBOL_EXPORT VST_SYMBOL_EXTERN __attribute__((visibility("default")))
+#endif
+
 #if defined (__GNUC__) && ((__GNUC__ >= 4) || ((__GNUC__ == 3) && (__GNUC_MINOR__ >= 1)))
     #define VST_EXPORT  __attribute__ ((visibility ("default")))
 #else
