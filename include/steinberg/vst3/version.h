15d9d69ed914e74457eaea6ba32c8f55789767d3
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-12-25 02:50:37 +0300

    Working on VST3 SDK adaptation

---
+++ include/steinberg/vst3/version.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2023 Linux Studio Plugins Project <lsp.plugin@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _3RD_PARTY_INCLUDE_STEINBERG_VST3_VERSION_H_
+#define _3RD_PARTY_INCLUDE_STEINBERG_VST3_VERSION_H_
+
+// The compatibility version of official VST3 SDK
+#define VST3_VERSION_MAJOR      3
+#define VST3_VERSION_MINOR      7
+#define VST3_VERSION_MICRO      9
+
+#endif /* _3RD_PARTY_INCLUDE_STEINBERG_VST3_VERSION_H_ */
