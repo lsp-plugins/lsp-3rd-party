11b23541b958073d4cdf781ad6a1d2b3724b3b33
Vladimir Sadovnikov <sadko4u@gmail.com>
2024-05-21 02:27:05 +0300

    Version up

--- include/lsp-plug.in/3rdparty/version.h
+++ include/lsp-plug.in/3rdparty/version.h
@@ -26,7 +26,7 @@
 // Version of headers
 #define LSP_3RD_PARTY_MAJOR             1
 #define LSP_3RD_PARTY_MINOR             0
-#define LSP_3RD_PARTY_MICRO             16
+#define LSP_3RD_PARTY_MICRO             17
 
 #ifdef LSP_COMMON_LIB_BUILTIN
     #define LSP_3RD_PARTY_EXPORT
