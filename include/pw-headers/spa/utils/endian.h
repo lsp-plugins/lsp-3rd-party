496dc9d705c4e2ba7c9115cd13a01467ce6d92b7
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-06-12 03:01:16 +0300

    Patch for FreeBSD

--- include/pw-headers/spa/utils/endian.h
+++ include/pw-headers/spa/utils/endian.h
@@ -5,7 +5,7 @@
 #ifndef SPA_ENDIAN_H
 #define SPA_ENDIAN_H
 
-#if defined(__MidnightBSD__)
+#if defined(__MidnightBSD__) || defined(__FreeBSD__)
 #include <sys/endian.h>
 #define bswap_16 bswap16
 #define bswap_32 bswap32
